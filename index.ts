026f98466b250d1178beb0b90f308a6515ce3bb8
Harrison Goldstein <hgoldstein95@gmail.com>
2019-02-23 21:58:46 -0500

    Fixed library setup

---
+++ index.ts
@@ -0,0 +1,14 @@
+import tourist from "./src/tourist";
+
+export default {
+  add: tourist.add,
+  check: tourist.check,
+  edit: tourist.edit,
+  get: tourist.get,
+  init: tourist.init,
+  move: tourist.move,
+  refresh: tourist.refresh,
+  remove: tourist.remove,
+  resolve: tourist.resolve,
+  scramble: tourist.scramble,
+};
